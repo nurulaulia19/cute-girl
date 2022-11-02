87ac14d663ad97ba38299712b9723610da281ee7
nurulaulia19 <nurulauliaseptiani9@gmail.com>
2022-11-02 16:22:22 +0700

    Inisialisasi resep cute-girl

---
+++ Resep.java
@@ -0,0 +1,12 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Resep{
+    public static void main(String[] args) {
+    System.out.println("resep krabby patty");
+    System.out.println("===================");
+    System.out.println("1. Bawang");
+    System.out.println("2. Ketimun");
+    }
+    
+
+}
